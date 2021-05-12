58eb2df9a96a5cf44aae8ef6dfcac98cd25499f5
Divyansh Bajpai <divyanshbajpai11@gmail.com>
2021-05-12 23:59:11 +0530

    WIP:Answer fetch

--- server/src/main/java/com/iqas/server/controller/KbController.java
+++ server/src/main/java/com/iqas/server/controller/KbController.java
@@ -2,8 +2,10 @@ package com.iqas.server.controller;
 
 import com.iqas.server.model.DocumentModel;
 import com.iqas.server.model.KbModel;
+import com.iqas.server.service.AnswerService;
 import com.iqas.server.service.DocumentService;
 import com.iqas.server.service.KbService;
+import org.springframework.beans.AbstractNestablePropertyAccessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,9 @@ public class KbController {
 
     @Autowired
     private DocumentService documentService;
+
+    @Autowired
+    private AnswerService answerService;
 
     @RequestMapping("/hello")
     public String hello(){
@@ -36,5 +41,10 @@ public class KbController {
     @RequestMapping(value = "/api/kb/{id}/doc",method = RequestMethod.POST)
     public DocumentModel postDoc(@RequestBody DocumentModel documentModel, @PathVariable("id") Integer id){
         return documentService.addDocument(documentModel, id);
+    }
+
+    @RequestMapping(value="api/ar/kb/{kbId}/doc/{docId}",method=RequestMethod.POST)
+    public String fetchAnswer(@RequestBody String question,@PathVariable("kbId") Integer kbId,@PathVariable("docId") Integer docId){
+        return answerService.fetchAnswer(kbId,docId,question);
     }
 }
---
+++ server/src/main/java/com/iqas/server/service/AnswerService.java
@@ -0,0 +1,12 @@
+package com.iqas.server.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnswerService {
+
+    public String fetchAnswer(Integer kbId, Integer docId, String question){
+        return  ""; //WIP
+    }
+
+}
